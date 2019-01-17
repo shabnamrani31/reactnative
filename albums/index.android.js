2d84f57706aa535d25f0a1f497c2c0a0dc6fddfe
Shabnam Rani <shabnamrani31@gmail.com>
2019-01-17 18:44:08 +0500

    react native

---
+++ albums/index.android.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+<Text>some text</Text>
+);
+AppRegistry.registerComponent(albums, () => App);
+
+/*
+import React,{component}from 'react'
+
+import {Text,
+    AppRegistry,
+    StyleSheet,
+    View
+} from 'react-native';
+class test1 extends Component
+{
+
+
+render(){
+    return
+        <View style ={styles.container}/>
+        <Text style ={styles.Welcome}>
+        welcome to react Native 
+        </Text>
+         <Text style ={styles.instructions}>
+         to get statrte edit 
+         </Text>
+    
+}
+}
+*/
